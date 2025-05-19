329234968fd14d3f014a1464294614a9fa2f4c29
facuagueria <fagueria@sugit.com.ar>
2025-05-19 11:09:09 -0300

    feat(ui): add Toaster component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { Toaster } from '@/components/ui/sonner'
 </script>
 
 <template>
   <main class="min-h-screen bg-gray-50">
+    <Toaster rich-colors />
+
     <RouterView />
   </main>
 </template>
